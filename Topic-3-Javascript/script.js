7568fd0416a8e5e0b5c3d4fa133a6684e4304c8a
Bruno <brunorios2009@hotmail.com>
2019-07-10 16:50:43 -0300

    1_Ab phrase palindrome

--- Topic-3-Javascript/script.js
+++ Topic-3-Javascript/script.js
@@ -2,26 +2,13 @@ let btn = document.querySelector('button');
 
 btn.onclick = function () {
     let txt = document.getElementById("idInput").value;
-    wordLength = txt.length;
-    if (txt.contains)
+    palindrome(txt);
 
-    if (wordLength%2 == 0){
-        middle = wordLength/2;
-        let half1 = txt.slice(0, middle);
-        let half2 = txt.slice(middle, wordLength);
-        palindrome(half1, half2);
-    }else{
-        middle = parseInt(wordLength/2);
-        console.log(middle);
-        let half1 = txt.slice(0, middle);
-        let half2 = txt.slice(middle+1, wordLength);
-        palindrome(half1, half2);
-    }
+    function palindrome(str) {  
+        let cleanStr = str.replace(/[^A-Z0-9]/ig,'');
+        let reverseStr = cleanStr.split('').reverse().join('');
 
-    function palindrome(str1, str2) {        
-        let reverseStr = str2.split('').reverse().join('');        
-        console.log(reverseStr);  
-        if(reverseStr == str1){
+        if(reverseStr == cleanStr){
             document.getElementById("output").value = "It\'s a palindrome";            
         }else{
             document.getElementById("output").value = "It\'s not a palindrome";
