2835745a7afdaddd0ae62c5c20e391d2350c748f
Bruno <brunorios2009@hotmail.com>
2019-07-12 02:00:35 -0300

    3_A started, can not obtain the joke

--- Topic-3-Javascript/script.js
+++ Topic-3-Javascript/script.js
@@ -1,9 +1,26 @@
 let btn = document.querySelector("button");
+let req = new XMLHttpRequest();
+let pp;
 
-btn.onclick = function () {
+/*btn.onclick = function () {
     fnAlert();
 }
 
 function fnAlert () {
     alert("You clicked me!");
+}*/
+btn.onclick = function () {
+    fnGetData();
+}
+
+function fnGetData() {
+    req.open ("GET","http://api.icndb.com/jokes/random");
+
+    req.addEventListener("load", (data) => {
+        pp =(JSON.parse(data.target.response));
+        /*document.getElementById("jokeNorris").value = pp;*/
+        console.log(pp);
+    });
+
+    req.send();
 }
