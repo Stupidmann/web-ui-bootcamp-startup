95a6e95510360da9383c1060ee255b76f4056392
Bruno <brunorios2009@hotmail.com>
2019-07-13 23:06:47 -0300

    3_A fetch jokes from api

--- Topic-3-Javascript/script.js
+++ Topic-3-Javascript/script.js
@@ -2,25 +2,32 @@ let btn = document.querySelector("button");
 let req = new XMLHttpRequest();
 let pp;
 
-/*btn.onclick = function () {
-    fnAlert();
-}
-
-function fnAlert () {
-    alert("You clicked me!");
-}*/
 btn.onclick = function () {
-    fnGetData();
+    fetch("http://api.icndb.com/jokes/random")
+        .then(res => res.json())
+        .then (data => {
+            console.log(data.value.joke);
+            pp = data.value.joke;
+            document.getElementById("jokeNorris").value = pp;
+        })
+        .catch(err => {
+            console.log("error!");
+            console.error(err);
+            fnRedSection();
+        })
+    function fnRedSection () {
+        document.body.style.backgroundColor = "red";
+        document.getElementById("jokeNorris").value = "ERROR!"
+    }
 }
 
-function fnGetData() {
+/*function fnGetData() {
     req.open ("GET","http://api.icndb.com/jokes/random");
 
     req.addEventListener("load", (data) => {
-        pp =(JSON.parse(data.target.response));
-        /*document.getElementById("jokeNorris").value = pp;*/
-        console.log(pp);
+        pp = (JSON.parse(data.target.response));
+        
     });
 
     req.send();
-}
+}*/
