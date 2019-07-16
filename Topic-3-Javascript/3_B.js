4f15eb9f6ddf80d675c777beb0dcf0e47070522c
Bruno <brunorios2009@hotmail.com>
2019-07-15 23:33:33 -0300

    3_B list done but not with <ul>

--- Topic-3-Javascript/3_B.js
+++ Topic-3-Javascript/3_B.js
@@ -1,12 +1,20 @@
 let btn = document.querySelector("button");
 let url = "https://api.github.com/search/repositories?q=javascript";
+const divHtml = document.getElementById("divHtml");
 
 btn.addEventListener("click", function() {
     async function API () {
         const baseUrl = url;
         const res = await fetch(baseUrl);
         const data = await res.json();
-        return data; 
+
+        return data.items.forEach(i => {
+            const anchor = document.createElement("a")
+            anchor.href = i.html_url
+            anchor.textContent = i.full_name;
+            divHtml.appendChild(anchor)
+            divHtml.appendChild(document.createElement("br"))
+        })
     }
-    API().then(console.log);
+    API();
 })
