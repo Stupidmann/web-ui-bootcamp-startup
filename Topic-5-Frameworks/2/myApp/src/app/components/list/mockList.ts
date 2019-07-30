deb5d16f328f2bce7c46875b9bea67861d0161a5
Bruno <brunorios2009@hotmail.com>
2019-07-30 02:09:04 -0300

    2 done

---
+++ Topic-5-Frameworks/2/myApp/src/app/components/list/mockList.ts
@@ -0,0 +1,9 @@
+import { List } from './list';
+
+export const LIST: List[] = [
+  { id: 1, name: 'Batman Begins', year:2005, duration:140 },
+  { id: 2, name: 'Django Unchained',year:2012, duration:165 },
+  { id: 3, name: 'The Godfather',year:1972, duration:178 },
+  { id: 4, name: 'Pulp Fiction',year:1994, duration:178 },
+  { id: 5, name: 'Fury', year:2014, duration:135},
+];
